da98ef53543ccbe045b223b3a5ae9e7479e7a887
Ibrahim Hossain <ih75754@gmail.com>
2021-11-21 23:35:23 +0600

    image insert & show client site

--- src/pages/AllProducts/AllProducts.js
+++ src/pages/AllProducts/AllProducts.js
@@ -22,7 +22,7 @@ const AllProducts = () => {
                         products.map(product => <div className="col text-start">
                             <div className="card h-100 border-0 card-custom">
                                 <div className="card-img">
-                                    <img src={product.img} className="card-img-top img-height p-3" alt="..." />
+                                    <img src={`data:image/png;base64,${product.img}`} className="card-img-top img-height p-3" alt="..." />
                                 </div>
                                 <div className="card-body">
                                     <h5 className="card-title">{product.name}</h5>
--- src/pages/Dashboard/AdminDashboard/AddProduct/AddProduct.js
+++ src/pages/Dashboard/AdminDashboard/AddProduct/AddProduct.js
@@ -5,9 +5,14 @@ import { useForm } from 'react-hook-form';
 const AddProduct = () => {
     const { register, handleSubmit, reset } = useForm();
     const onSubmit = data => {
-        // console.log(data);
+        const formData = new FormData();
+        formData.append('name', data.name);
+        formData.append('description', data.description);
+        formData.append('price', data.price);
+        formData.append('img', data.img[0]);
+
         // use AXIOS for post into data base
-        axios.post('https://agile-everglades-07523.herokuapp.com/products', data)
+        axios.post('http://localhost:5000/products', formData)
             .then(res => {
                 // console.log(res);
                 if (res.data.insertedId) {
@@ -18,13 +23,13 @@ const AddProduct = () => {
     };
     return (
         <div>
-            <h1>add A Product</h1>
+            <h1>Add A Product</h1>
             <div>
                 <form onSubmit={handleSubmit(onSubmit)} className="d-flex flex-column justify-content-center align-items-center mt-5">
-                    <input className="w-50 mb-3 p-2"  {...register("img")} type="text" placeholder="image URL" required />
                     <input className="w-50 mb-3 p-2" {...register("name")} type="name" placeholder="Package Name" required />
                     <textarea className="w-50 mb-3 p-2"  {...register("description")} type="text" placeholder="Description" required />
                     <input className="w-50 mb-3 p-2"  {...register("price")} type="number" placeholder="Price" required />
+                    <input className="w-50 mb-3 p-2"  {...register("img")} accept="image/*" type="file" required />
                     <input className="btn btn-success w-50 mb-3 p-2" type="submit" />
                 </form>
             </div>
